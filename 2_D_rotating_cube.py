4fb2f715405af33622745002f0bebe9fbc11b1e2
BenjaminWills <benjaminwills047@gmail.com>
2022-11-21 22:29:30 +0000

    Lines between points plottable now.

--- 2_D_rotating_cube.py
+++ 2_D_rotating_cube.py
@@ -36,18 +36,22 @@ def translate_grid(grid:np.array) -> np.array:
     vectorised_grid = np.vectorize(translate_element)
     return vectorised_grid(grid)
 
-def scale_axes(output:float,grid_shape:tuple) -> int:
-    x,y = grid_shape
-    dy = 1/y
-    points = [dy * i for i in range(y)]
-    for i in range(1,len(points)):
-        if points[i-1] < output <= points[i]:
-            return i
-    return None
+#################### ARCHIVE #########################
 
+# def scale_axes(output:float,grid_shape:tuple) -> int:
+#     x,y = grid_shape
+#     dy = 1/y
+#     points = [dy * i for i in range(y)]
+#     for i in range(1,len(points)):
+#         if points[i-1] < output <= points[i]:
+#             return i
+#     return None
+
+######################################################
 
 def plot_line(start:np.array,end:np.array,grid:np.array) -> np.array:
-    x,y = grid.shape
+    shape = grid.shape
+    x,y = shape
 
     dx = 1/x# one step on the x axis
     dy = 1/y # one step on the y axis
@@ -69,11 +73,12 @@ def plot_line(start:np.array,end:np.array,grid:np.array) -> np.array:
     x_0,x_1 = x_0 * dx, x_1 * dx
     y_0,y_1 = y_0 * dy, y_1 * dy
 
-    gradient = np.floor((y_0-y_1)/(x_0-x_1))
-    line = lambda x : np.floor(gradient * (x-x_1) + y_1 * dy)
+    gradient = (y_0-y_1)/(x_0-x_1)
+    line = lambda x : gradient * (x-x_1) + y_1
     for i in range(x):
-        y_i = line(i * dx)
-        if y_i > y:
+        line_output = int(line(i))
+        y_i = line_output
+        if  y_i is None:
             continue
         else:
             grid[i][y_i] = 1
@@ -83,8 +88,8 @@ def plot_line(start:np.array,end:np.array,grid:np.array) -> np.array:
 if __name__ == "__main__":
     grid = get_grid((5,5))
 
-    start = np.array([0,0])
-    end = np.array([0,4])
+    start = np.array([4,4])
+    end = np.array([0,0])
 
     grid = plot_line(start,end,grid)
 
