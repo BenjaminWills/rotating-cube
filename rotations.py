9ea2d532b5e445960c5523359699bec5e3cec459
Benjaminwills <benjaminwills047@gmail.com>
2022-11-27 18:54:59 +0000

    Organising

---
+++ rotations.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def rotation_matrix(angle:float) -> np.array:
+    """Rotates a vector by a angle radians
+
+    Parameters
+    ----------
+    angle : float
+        Number of radians
+
+    Returns
+    -------
+    np.array
+        Rotation matrix for that angle.
+    """
+    cosine = np.cos(angle)
+    sine = np.sin(angle)
+    return np.array([[cosine,sine],[-sine,cosine]])
+
+def rotate_point(point:np.array,angle:float) -> np.array:
+    """Rotates a point by some angle
+
+    Parameters
+    ----------
+    point : np.array
+    angle : float
+
+    Returns
+    -------
+    np.array
+        rotated vector
+    """
+    return np.matmul(rotation_matrix(angle),point)
